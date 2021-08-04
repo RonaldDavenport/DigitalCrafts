f7797f27168a0c2d97f27163560b368043fb5d21
ronald davenport <ronalddavenport08@gmail.com>
2021-08-04 16:07:49 -0500

    Determine whether even or odd

---
+++ week1/day3/EvenOdd.py
@@ -0,0 +1,5 @@
+print ("Find out if your number is even or odd!!!")
+num1=input("Enter a number of your choice")
+if (int(num1) / 2) == 0:
+      print("Your number is even")  
+else: print("Your number is odd")
