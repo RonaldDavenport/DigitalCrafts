b116fb72b53ab15333f665d313004dc473960ed0
ronald davenport <ronalddavenport08@gmail.com>
2021-08-04 16:07:24 -0500

    calculator assignment

---
+++ week1/day3/Calculator.py
@@ -0,0 +1,16 @@
+print("Welcome to my calculator")
+
+num1=input("Type any number!!")
+symbol=input("Enter any math symbol")
+num2=input("Type another number!!")
+
+
+if symbol == ("+") :
+        print(int(num1)+int(num2))
+
+if symbol == ("-") :
+         print(int(num1)-int(num2))
+if symbol == ("/") :
+         print((int)(num1)/int(num2))  
+if symbol == ("*") :
+         print(int(num1)*int(num2))           
