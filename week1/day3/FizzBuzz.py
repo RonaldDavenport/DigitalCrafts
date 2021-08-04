46d928fe42a3410146e41fa1382c317b4ecaad1d
ronald davenport <ronalddavenport08@gmail.com>
2021-08-04 16:07:58 -0500

    fizzbuzz

---
+++ week1/day3/FizzBuzz.py
@@ -0,0 +1,13 @@
+print("Welcome to FizzBuzz!!")
+
+num1= int(input("Enter any number of your choice!"))
+
+if (num1 % 3) ==0 and (num1 % 5)== 0:
+    print("fizzbuzz")
+
+if  (num1) % 3 == 0:
+      print("fiz")
+
+elif (num1) % 5  == 0:
+      print("buzz")
+
