445364a28a7348470ac87a70ae919776fb0367a8
ronald davenport <ronalddavenport08@gmail.com>
2021-11-19 11:34:55 -0600

    footlocker

--- week13/day1/footlockerbackend.js
+++ week13/day1/footlockerbackend.js
@@ -2,6 +2,30 @@ const { createClient } = require('@supabase/supabase-js')
 const express = require("express")
 const app = express();
 const PORT = 5000
+const path = require('path');
+
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.heroku...']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
+
+
+
+
+
+
+
 const shoeData = [
     {
         "brand": "Jordan",
@@ -246,6 +270,16 @@ app.get("/grabData", async (req,res)=>{
   res.send(console.log(data))
 
 })    
+
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 
 app.listen(PORT,()=>console.log('listening on port' ))
